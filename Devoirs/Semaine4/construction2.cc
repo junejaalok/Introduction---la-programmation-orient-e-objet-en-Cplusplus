616029d1eac52f73386c8ead8c0c961d7712635e
Alok Juneja <junejaalok@gmail.com>
2015-04-01 23:29:33 +0200

    20150301_1_from home

--- Devoirs/Semaine4/construction2.cc
+++ Devoirs/Semaine4/construction2.cc
@@ -13,38 +13,57 @@ protected:
 	
 public:
 	Piece (string nm) : name(nm) {};
+  ostream& afficher(ostream& os) const ;
+};
 
-  ostream& afficher (ostream& os) const {
+  ostream& Piece :: afficher (ostream& os) const {
     os << '"' << name << '"';
     return os;
   }
-	
-};
+
 
 class Simple : public Piece {
 protected:
 	string orient;
 public:
 	Simple (string nm,string orint=""):Piece(nm),orient(orint){};
-    
-  ostream& afficher (ostream& os) const {
-          os << "pièce simple" << '"' << "name" << '"';
-        return os;
-    }
-	
-
-
+  friend ostream& operator<<(ostream&, const Simple&);
 };
-/*
-class Composee : private Piece{
-public:
-	Composee (string nm){};
-	static construire (const Simple& sm) {
-		
 
+  ostream& operator<< (ostream& os,const Simple& other) {
+    os << "pièce simple ";
+    other.afficher(os);
+    if (!other.orient.empty())
+      os << " orienté vers la " << other.orient << endl;
+    else
+      os << endl;
+    return os;
+  }
+
+
+class Composee : public Piece{
+private:
+  vector <Simple> svec;
+
+public:
+	Composee (string nm):Piece(nm) {};
+
+  friend ostream& operator<<(ostream&, const Composee&);
+	void construire (const Simple& sm) {
+		svec.push_back(sm);
 	}
 };
-*/
+
+  ostream& operator<< (ostream& os,const Composee& other) {
+    os << "pièce ";
+    other.afficher(os);
+    os << " composée de :" << endl;
+    for (vector<Simple>::const_iterator it = other.svec.begin() ; it != other.svec.end(); ++it) {
+      os << "    * ";
+      os << *it;
+    }
+    return os;
+  }
 
 /*******************************************
  * Ne rien modifier après cette ligne.
@@ -65,8 +84,8 @@ int main()
   ajouterComposant(Simple("brique standard"), 59);
 
   // Déclare une pièce composée dont le nom est «porte»
-//  Composee porte("porte");
-/*
+  Composee porte("porte");
+
   // Une porte est constituée...
   //  ...d'un cadran de porte orienté à gauche
   porte.construire(Simple("cadran porte", "gauche"));
@@ -89,6 +108,6 @@ int main()
 
   // La boite de jeu a en plus deux fenêtres
   ajouterComposant(fenetre, 2);
-*/
+
   return 0;
 }
